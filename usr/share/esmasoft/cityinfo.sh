22ec962e6d476d6b5e199510657afddf714285f6
Mikhail Paulyshka <me@mixaill.tk>
2016-06-25 10:38:07 -0700

    r10: disable wine gecko, disable wine mono, add progress indicator, check installer integrity, fix dependencies

--- usr/share/esmasoft/cityinfo.sh
+++ usr/share/esmasoft/cityinfo.sh
@@ -15,7 +15,7 @@
 # Отключенные:
 # comctl32 : устраняет одни глитчи, но добавляет другие. Спорный момент
 
-export CI_VERSION=9
+export CI_VERSION=10
 
 export CI_DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CI_DIR_ROOT="$HOME/.esmasoft/cityinfo"
@@ -31,6 +31,71 @@ export URL_CITYINFO_FILENAME="cityinfo3073.exe"
 export URL_LIBKASNERIK="https://github.com/Mixaill/libKasnerik/releases/download/0.0.1/libKasnerik_0.0.1.zip"
 export URL_WINETRICKS="https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
 
+#Winetricks
+winetricks_parse_wget_progress()
+{
+    # Parse a percentage, a size, and a time into $1, $2 and $3
+    # then use them to create the output line.
+    perl -p -e \
+       '$| = 1; s/^.* +([0-9]+%) +([0-9,.]+[GMKB]) +([0-9hms,.]+).*$/\1\n# Downloading... \2 (\3)/'
+}
+
+winetricks_get_sha1sum()
+{
+    local _W_file="$1"
+
+    if [ -f "$_W_file" ] || [ -h "$_W_file" ]
+    then
+        _W_gotsum=`sha1sum < "$_W_file" | sed 's/(stdin)= //;s/ .*//'`
+    else
+        return
+    fi
+}
+
+winetricks_wget_progress()
+{
+	# Usa a subshell so if the user clicks 'Cancel',
+	# the --auto-kill kills the subshell, not the current shell
+	(
+	    wget "$@" 2>&1 |
+	    winetricks_parse_wget_progress | \
+	    zenity --progress --no-cancel --width 400 --title="$_W_title" --auto-kill --auto-close
+	)
+	err=$?
+	if test $err -gt 128
+	then
+	    # 129 is 'killed by SIGHUP'
+	    # Sadly, --auto-kill only applies to parent process,
+	    # which was the subshell, not all the elements of the pipeline...
+	    # have to go find and kill the wget.
+	    # If we ran wget in the background, we could kill it more directly, perhaps...
+	    if pid=`ps augxw | grep ."$_W_file" | grep -v grep | awk '{print $2}'`
+	    then
+		echo User aborted download, killing wget
+		kill $pid
+	    fi
+	fi
+	return $err
+
+}
+
+winetricks_download()
+{
+	_W_url="$1"
+	_W_file="$2"
+	_W_title="$3"
+	_W_checksum="$4"
+
+	winetricks_get_sha1sum
+
+        if [ "$_W_gotsum"x != "$_W_checksum"x ] || [ "$_W_checksum" = "" ];
+        then
+		winetricks_wget_progress -O "$_W_file" -nd -c --read-timeout=300 --retry-connrefused "$_W_url"
+	fi
+}
+#
+
+#
 directories_prepare()
 {
 	rm -rf "$CI_DIR_WINEPRFIX/"
@@ -50,30 +115,28 @@ directories_clean()
 
 wine_createPrefix()
 {
-	WINEPREFIX="$CI_DIR_WINEPRFIX" WINEARCH="win32" wineboot
+	WINEPREFIX="$CI_DIR_WINEPRFIX" WINEARCH="win32" WINEDLLOVERRIDES="mscoree,mshtml=" wineboot
 }
 
 wine_winetricks()
 {
 	cd "$CI_DIR_TEMP"
-	wget "$URL_WINETRICKS"
+	winetricks_download "$URL_WINETRICKS" "./winetricks" "Downloading winetricks"
 	chmod +x ./winetricks
-	#WINEPREFIX="$CI_DIR_WINEPRFIX" ./winetricks comctl32 
-	WINEPREFIX="$CI_DIR_WINEPRFIX" ./winetricks ie7 
-	WINEPREFIX="$CI_DIR_WINEPRFIX" ./winetricks riched20
+
+	#ie7
+	winetricks_download "http://download.microsoft.com/download/3/8/8/38889DC1-848C-4BF2-8335-86C573AD86D9/IE7-WindowsXP-x86-enu.exe" "$HOME/.cache/winetricks/ie7/IE7-WindowsXP-x86-enu.exe" "Downloading IE7" "d39b89c360fbaa9706b5181ae4718100687a5326"
+	WINEPREFIX="$CI_DIR_WINEPRFIX" ./winetricks --force --optout --unattended ie7
+
+	#riched20
+	winetricks_download "https://web.archive.org/web/20160129053851/http://download.microsoft.com/download/E/6/A/E6A04295-D2A8-40D0-A0C5-241BFECD095E/W2KSP4_EN.EXE" "$HOME/.cache/winetricks/win2ksp4/W2KSP4_EN.EXE" "Downloading Windows 2000 SP4" "fadea6d94a014b039839fecc6e6a11c20afa4fa8"
+	WINEPREFIX="$CI_DIR_WINEPRFIX" ./winetricks --force --optout --unattended riched20
 }
 
 cityInfo_download()
 {
-	cd "$CI_DIR_TEMP"
-
-	if [ ! -f "$CI_DIR_CACHE/$URL_CITYINFO_FILENAME" ]; then
-		wget "$URL_CITYINFO"
-		cp "$CI_DIR_TEMP/$URL_CITYINFO_FILENAME" "$CI_DIR_CACHE/$URL_CITYINFO_FILENAME"
-	else
-		cp "$CI_DIR_CACHE/$URL_CITYINFO_FILENAME" "$CI_DIR_TEMP/$URL_CITYINFO_FILENAME"
-	fi
-	
+	winetricks_download "$URL_CITYINFO" "$CI_DIR_CACHE/$URL_CITYINFO_FILENAME" "Downloading CityInfo" "43299557dda6668dd2934d86dbafc92232e099a4"
+	cp "$CI_DIR_CACHE/$URL_CITYINFO_FILENAME" "$CI_DIR_TEMP/$URL_CITYINFO_FILENAME"
 }
 
 cityInfo_unpackAndCopy()
@@ -93,7 +156,7 @@ cityInfo_bugfix()
 libKasnerik_download()
 {
 	cd "$CI_DIR_TEMP"
-	wget "$URL_LIBKASNERIK"
+	winetricks_download "$URL_LIBKASNERIK" "./libKasnerik_0.0.1.zip" "Downloading LibKasnerik" ""
 }
 
 libKasnerik_unpackAndCopy()
@@ -143,7 +206,7 @@ main()
 	then 
 	    installation
 	fi
-	
+
 	run
 }
 
